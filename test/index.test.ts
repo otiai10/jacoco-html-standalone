dc779e0a4b81a5190117694fdb92debada49ebc4
Hiromu OCHIAI <otiai10@gmail.com>
2022-01-04 18:18:48 +0900

    Add more assertion for converting

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,4 +1,15 @@
+import * as fs from "fs/promises";
+import { JSDOM } from "jsdom";
+
 import { convertDir } from "../src/lib"
+
+beforeEach(async () => {
+    await fs.rm("test/output", { recursive: true, force: true });
+});
+
+afterAll(async () => {
+    // await fs.rm("test/output", { recursive: true, force: true });
+});
 
 test("convert", async () => {
     var __out = "", __err = "";
@@ -6,6 +17,11 @@ test("convert", async () => {
         stdout: { write: function(str: string) { __out += str; return true; }},
         stderr: { write: function(str: string) { __err += str; return true; }},
     };
+
+    const input = await fs.readFile("test/example/jacocoHtml/index.html");
+    const orig = (new JSDOM(input)).window.document.querySelector("link[rel='shortcut icon']");
+    expect(orig?.getAttribute("href")).toBe("jacoco-resources/report.gif");
+
     await convertDir(
         "test/example/jacocoHtml",
         "test/example/jacocoHtml/jacoco-resources",
@@ -20,4 +36,8 @@ test("convert", async () => {
         "SEMAPHORE:	128\n",
     ].join("\n"))
     expect(__err.length).toBe(6);
-})
+
+    const output = await fs.readFile("test/output/index.html");
+    const dest = (new JSDOM(output)).window.document.querySelector("link[rel='shortcut icon']");
+    expect(dest?.getAttribute("href")?.startsWith("data:image/gif;base64")).toBeTruthy();
+});
